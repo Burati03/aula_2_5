c69f1aa57f374b4d655ca318eed19560e27d384a
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-06-23 15:59:19 +0000

    sfsfsf

---
+++ objetos/objeto1.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    raça: "Golden",
+    nome: "Enzo",
+    altura: "60cm",
+    largura: "100cm",
+    mudarCanal: function() {
+        console.log(cachorro.raça);
+    }
+};
+cachorro.mudarCanal();
+
