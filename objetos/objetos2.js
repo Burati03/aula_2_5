c262097a8ac3f15c1606726c8af9b617eda79f31
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-06-23 16:17:22 +0000

    vdsgsd

---
+++ objetos/objetos2.js
@@ -0,0 +1,14 @@
+const cachorro = {
+    tutor: {
+    nome: "Paulo",
+    },
+    raça: "Golden",
+    nome: "Enzo",
+    altura: "60cm",
+    largura: "100cm",
+    mudarCanal: function() {
+        console.log(cachorro.raça);
+    }
+};
+cachorro.mudarCanal();
+
